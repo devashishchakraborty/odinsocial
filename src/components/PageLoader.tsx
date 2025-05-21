0a35f28c650cc0d635bba1921c530a17efce01b3
Devashish Chakraborty <devashishchakra@gmail.com>
2025-05-22 01:04:35 +0530

    A fullscreen page loader

---
+++ src/components/PageLoader.tsx
@@ -0,0 +1,9 @@
+import Loading from "./Loading";
+
+export default function PageLoader() {
+  return (
+    <main className="h-full w-full flex items-center justify-center">
+      <Loading />
+    </main>
+  );
+}
