a9b2437244d8074ded70c5aa6564f36d2f6bbed3
Devashish Chakraborty <devashishchakra@gmail.com>
2025-05-22 00:15:44 +0530

    Created a basic routing setup with public and protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
+import { useContext } from "react";
 import "./App.css";
 import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+import Home from "./pages/Home";
+import PageLoader from "./components/PageLoader";
 
+const PublicRoute = () => {
+  const { isAuthenticated, isAuthenticating } = useContext(AuthContext);
+
+  return isAuthenticated ? (
+    <Navigate to="/" replace />
+  ) : isAuthenticating ? (
+    <PageLoader />
+  ) : (
+    <Outlet />
+  );
+};
+
+const ProtectedRoute = () => {
+  const { isAuthenticated, isAuthenticating } = useContext(AuthContext);
+
+  return isAuthenticated ? (
+    <Outlet />
+  ) : isAuthenticating ? (
+    <PageLoader />
+  ) : (
+    <Navigate to="/login" replace />
+  );
+};
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/sign-up" element={<SignUp />} />
+      <Route element={<PublicRoute />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Route>
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   );
 }
