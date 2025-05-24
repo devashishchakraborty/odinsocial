d811e60f576e8c1abc9f5294513eebb5cd8bce84
Devashish Chakraborty <devashishchakra@gmail.com>
2025-05-25 01:11:09 +0530

    Made the following and all section functioning

--- src/components/MainFeed.tsx
+++ src/components/MainFeed.tsx
@@ -16,14 +16,16 @@ import {
   ArrowPathRoundedSquareIcon as RepostIcon,
 } from "@heroicons/react/24/outline";
 
-
 const MainFeed = () => {
-  const [posts, setPosts] = useState<Post[]>();
+  const [posts, setPosts] = useState<Post[] | null>(null);
   const [error, setError] = useState(null);
+  const [showFollowingPosts, setShowFollowingPosts] = useState(false);
   const navigate = useNavigate();
   const { refreshToken, decodeAndCheckExpiry, user } = useContext(AuthContext);
+
   useEffect(() => {
     const fetchPosts = async () => {
+      setPosts(null);
       const token = localStorage.getItem("auth_token");
       if (!token) return;
 
@@ -35,7 +37,7 @@ const MainFeed = () => {
 
       try {
         const response = await fetch(
-          `${import.meta.env.VITE_API_BASE_URL}/posts`,
+          `${import.meta.env.VITE_API_BASE_URL}/posts?following=${showFollowingPosts}`,
           {
             method: "GET",
             headers: {
@@ -55,7 +57,7 @@ const MainFeed = () => {
       }
     };
     fetchPosts();
-  }, []);
+  }, [showFollowingPosts]);
 
   const handleUserClick = (e: MouseEvent, authorId: number) => {
     e.stopPropagation(); // Prevent parent <Link> navigation
@@ -79,7 +81,7 @@ const MainFeed = () => {
     }
 
     setPosts((posts) => {
-      return posts?.map((post) => {
+      return posts!.map((post) => {
         if (post.id !== postId) return post;
 
         const updatedPost = { ...post };
@@ -129,111 +131,128 @@ const MainFeed = () => {
   return (
     <section className="w-2xl border-x-1 border-gray-400">
       <section className="flex border-b-1 border-gray-400">
-        <div className="flex-1 border-r-1 border-gray-400 p-4 text-center transition-colors duration-200 hover:cursor-pointer hover:bg-gray-200">
+        <div
+          className={`relative flex-1 border-r-1 border-gray-400 p-4 ${showFollowingPosts || "font-bold"} text-center transition-colors duration-200 hover:cursor-pointer hover:bg-gray-200`}
+          onClick={() => setShowFollowingPosts(false)}
+        >
           All Posts
+          {showFollowingPosts || (
+            <div className="absolute bottom-1 left-1/2 h-1 w-15 -translate-x-1/2 rounded-full bg-sky-900"></div>
+          )}
         </div>
-        <div className="flex-1 p-4 text-center transition-colors duration-200 hover:cursor-pointer hover:bg-gray-200">
+        <div
+          className={`relative flex-1 p-4 text-center ${showFollowingPosts && "font-bold"} transition-colors duration-200 hover:cursor-pointer hover:bg-gray-200`}
+          onClick={() => setShowFollowingPosts(true)}
+        >
           Following
+          {showFollowingPosts && (
+            <div className="absolute bottom-1 left-1/2 h-1 w-15 -translate-x-1/2 rounded-full bg-sky-900"></div>
+          )}
         </div>
       </section>
       <section className="flex flex-col">
         {posts ? (
-          posts.map((post: Post) => {
-            const diff = getTimeDifference(post.createdAt);
-            return (
-              <section
-                onClick={() => navigate(`/posts/${post.id}`)}
-                key={post.id}
-                className="flex gap-2 border-b-1 border-gray-400 px-4 py-2 transition-colors duration-200 hover:cursor-pointer hover:bg-gray-100"
-              >
-                <div onClick={(e) => handleUserClick(e, post.author.id)}>
-                  <img
-                    src={post.author.profile.imageUrl || defaultPicture}
-                    alt="default profile"
-                    className="h-10 w-10 min-w-max"
-                  />
-                </div>
-                <div className="flex flex-1 flex-col gap-2">
-                  <div className="flex flex-col items-start">
-                    <div
-                      className="flex gap-1"
-                      onClick={(e) => handleUserClick(e, post.author.id)}
-                    >
-                      <div className="font-bold hover:underline">
-                        {post.author.name}
-                      </div>
-                      ·<div className="text-gray-800">{post.author.email}</div>·
-                      <div className="text-gray-800">{diff}</div>
-                    </div>
-                    <div>{post.content}</div>
+          posts.length === 0 ? (
+            <div className="p-4 text-center">No Posts Found!</div>
+          ) : (
+            posts.map((post: Post) => {
+              const diff = getTimeDifference(post.createdAt);
+              return (
+                <section
+                  onClick={() => navigate(`/posts/${post.id}`)}
+                  key={post.id}
+                  className="flex gap-2 border-b-1 border-gray-400 px-4 py-2 transition-colors duration-200 hover:cursor-pointer hover:bg-gray-100"
+                >
+                  <div onClick={(e) => handleUserClick(e, post.author.id)}>
+                    <img
+                      src={post.author.profile.imageUrl || defaultPicture}
+                      alt="default profile"
+                      className="h-10 w-10 min-w-max"
+                    />
                   </div>
-
-                  <div className="flex items-center justify-between text-gray-600">
-                    <div className="flex w-sm items-center justify-between gap-10">
+                  <div className="flex flex-1 flex-col gap-2">
+                    <div className="flex flex-col items-start">
                       <div
-                        className="flex items-center gap-1 hover:text-pink-700"
-                        title="Like"
+                        className="flex gap-1"
+                        onClick={(e) => handleUserClick(e, post.author.id)}
                       >
-                        {post.likedBy.some((obj) => obj.id === user!.id) ? (
-                          <LikeIcon
-                            className="h-5 w-5 text-pink-600"
-                            onClick={(e) =>
-                              handlePostUpdate(e, post.id, "false")
-                            }
-                          />
-                        ) : (
-                          <LikeIconOutline
+                        <div className="font-bold hover:underline">
+                          {post.author.name}
+                        </div>
+                        ·
+                        <div className="text-gray-800">{post.author.email}</div>
+                        ·<div className="text-gray-800">{diff}</div>
+                      </div>
+                      <div>{post.content}</div>
+                    </div>
+
+                    <div className="flex items-center justify-between text-gray-600">
+                      <div className="flex w-sm items-center justify-between gap-10 select-none">
+                        <div
+                          className="flex items-center gap-1 hover:text-pink-700"
+                          title="Like"
+                        >
+                          {post.likedBy.some((obj) => obj.id === user!.id) ? (
+                            <LikeIcon
+                              className="h-5 w-5 text-pink-600"
+                              onClick={(e) =>
+                                handlePostUpdate(e, post.id, "false")
+                              }
+                            />
+                          ) : (
+                            <LikeIconOutline
+                              className="h-5 w-5"
+                              onClick={(e) =>
+                                handlePostUpdate(e, post.id, "true")
+                              }
+                            />
+                          )}
+                          {post.likedBy.length}
+                        </div>
+
+                        <div
+                          className="flex items-center gap-1 hover:text-sky-700"
+                          title="Comment"
+                        >
+                          <CommentIcon
                             className="h-5 w-5"
-                            onClick={(e) =>
-                              handlePostUpdate(e, post.id, "true")
-                            }
+                            onClick={(e) => handlePostUpdate(e, post.id)}
                           />
-                        )}
-                        {post.likedBy.length}
+                          {post.comments.length}
+                        </div>
+                        <div
+                          className="flex items-center gap-1 hover:text-green-700"
+                          title="Repost"
+                        >
+                          <RepostIcon
+                            className="h-5 w-5"
+                            onClick={(e) => handlePostUpdate(e, post.id)}
+                          />
+                          {post.comments.length}
+                        </div>
                       </div>
 
-                      <div
-                        className="flex items-center gap-1 hover:text-sky-700"
-                        title="Comment"
-                      >
-                        <CommentIcon
-                          className="h-5 w-5"
-                          onClick={(e) => handlePostUpdate(e, post.id)}
+                      {post.bookmarkedBy.some((obj) => obj.id === user!.id) ? (
+                        <BookmarkIcon
+                          className="h-5 w-5 text-sky-600 hover:text-sky-700"
+                          onClick={(e) =>
+                            handlePostUpdate(e, post.id, null, "false")
+                          }
                         />
-                        {post.comments.length}
-                      </div>
-                      <div
-                        className="flex items-center gap-1 hover:text-green-700"
-                        title="Repost"
-                      >
-                        <RepostIcon
-                          className="h-5 w-5"
-                          onClick={(e) => handlePostUpdate(e, post.id)}
+                      ) : (
+                        <BookmarkIconOutline
+                          className="h-5 w-5 hover:text-sky-700"
+                          onClick={(e) =>
+                            handlePostUpdate(e, post.id, null, "true")
+                          }
                         />
-                        {post.comments.length}
-                      </div>
+                      )}
                     </div>
-
-                    {post.bookmarkedBy.some((obj) => obj.id === user!.id) ? (
-                      <BookmarkIcon
-                        className="h-5 w-5 text-sky-600 hover:text-sky-700"
-                        onClick={(e) =>
-                          handlePostUpdate(e, post.id, null, "false")
-                        }
-                      />
-                    ) : (
-                      <BookmarkIconOutline
-                        className="h-5 w-5 hover:text-sky-700"
-                        onClick={(e) =>
-                          handlePostUpdate(e, post.id, null, "true")
-                        }
-                      />
-                    )}
                   </div>
-                </div>
-              </section>
-            );
-          })
+                </section>
+              );
+            })
+          )
         ) : (
           <PageLoader />
         )}
