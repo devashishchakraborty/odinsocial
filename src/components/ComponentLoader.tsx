20f93a5a576b13fa6200d3beaa346c03f28083cb
Devashish Chakraborty <devashishchakra@gmail.com>
2025-05-26 03:34:29 +0530

    Added an additional Component Loader for specific loading animations for smaller components

---
+++ src/components/ComponentLoader.tsx
@@ -0,0 +1,9 @@
+import Loading from "./Loading";
+
+export default function ComponentLoader() {
+  return (
+    <main className="flex h-full w-full justify-center p-5">
+      <Loading />
+    </main>
+  );
+}
