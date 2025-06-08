e940cab75cd9a5c0b7b04d332913729658c361e4
Devashish Chakraborty <devashishchakra@gmail.com>
2025-06-08 23:48:58 +0530

    Added the user follow/unfollow functionality in the RightSideBar

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -1,5 +1,5 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import { useContext, useEffect, useState } from "react";
+import { MouseEvent, useContext, useEffect, useState } from "react";
 import { User } from "../types";
 import defaultPicture from "../assets/defaultPicture.png";
 import ComponentLoader from "./ComponentLoader";
@@ -9,7 +9,7 @@ import { AuthContext } from "../context/AuthContext";
 const RightSideBar = ({ currentUserId }: { currentUserId?: number }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [users, setUsers] = useState<User[] | null>(null);
-  const { getAuthHeaders } = useContext(AuthContext);
+  const { getAuthHeaders, user } = useContext(AuthContext);
   const [error, setError] = useState(null);
   useEffect(() => {
     const fetchUsers = async () => {
@@ -37,6 +37,53 @@ const RightSideBar = ({ currentUserId }: { currentUserId?: number }) => {
     fetchUsers();
   }, [currentUserId]);
 
+  const toggleFollow = async (
+    e: MouseEvent,
+    userId: number,
+    action: string,
+  ) => {
+    e.preventDefault();
+    if (!user || !userId) return;
+
+    setUsers((prev) => {
+      if (!prev) return prev;
+
+      return prev?.map((_user) => {
+        if (_user.id !== userId) return _user;
+
+        const updatedUser = { ..._user };
+
+        if (action === "FOLLOW") {
+          updatedUser.followers = [...updatedUser.followers!, { id: user!.id }];
+        } else if (action === "UNFOLLOW") {
+          updatedUser.followers = updatedUser.followers!.filter(
+            (follower) => follower.id !== user!.id,
+          );
+        }
+
+        return updatedUser;
+      });
+    });
+
+    try {
+      const headers = await getAuthHeaders();
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/users/${userId}/toggle-follow`,
+        {
+          method: "PUT",
+          headers: headers,
+          body: JSON.stringify({ action }),
+        },
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+    } catch (err: any) {
+      console.error("Error fetching data:", err);
+      setError(err.message);
+    }
+  };
+
   return (
     <section className="sticky top-0 h-screen overflow-y-auto p-4">
       <form className="relative mb-4">
@@ -54,26 +101,39 @@ const RightSideBar = ({ currentUserId }: { currentUserId?: number }) => {
       <section className="rounded-2xl border-1 border-gray-400">
         <h1 className="p-4 text-xl font-bold">Who to follow</h1>
         {users
-          ? users.map((user) => {
+          ? users.map((_user) => {
               return (
                 <Link
-                  to={`/user/${user.id}`}
-                  key={user.id}
-                  
+                  to={`/user/${_user.id}`}
+                  key={_user.id}
                   className="flex items-center gap-2 p-4 transition-colors duration-200 hover:cursor-pointer hover:bg-gray-100"
                 >
                   <img
-                    src={user.profile.imageUrl || defaultPicture}
-                    alt={user.name}
+                    src={_user.profile.imageUrl || defaultPicture}
+                    alt={_user.name}
                     className="h-10 w-10 min-w-max rounded-full"
                   />
                   <div>
-                    <div className="font-bold">{user.name}</div>
-                    <div className="text-gray-600">{user.email}</div>
+                    <div className="font-bold">{_user.name}</div>
+                    <div className="text-gray-600">{_user.email}</div>
                   </div>
-                  <button onClick={(e) => e.preventDefault()} className="ml-auto cursor-pointer rounded-3xl bg-sky-600 px-4 py-2 font-bold text-white hover:bg-sky-700">
-                    Follow
-                  </button>
+                  {_user.followers?.some(
+                    (follower) => follower.id === user!.id,
+                  ) ? (
+                    <button
+                      className="cursor-pointer rounded-3xl border-1 border-sky-900 bg-white px-4 py-2 font-bold text-sky-950 hover:bg-gray-100"
+                      onClick={(e) => toggleFollow(e, _user.id, "UNFOLLOW")}
+                    >
+                      Following
+                    </button>
+                  ) : (
+                    <button
+                      className="cursor-pointer rounded-3xl bg-sky-600 px-4 py-2 font-bold text-white hover:bg-sky-700"
+                      onClick={(e) => toggleFollow(e, _user.id, "FOLLOW")}
+                    >
+                      Follow
+                    </button>
+                  )}
                 </Link>
               );
             })
