30a852daae1e9b6a1d85a681aabf2b6dd8840055
Devashish Chakraborty <devashishchakra@gmail.com>
2025-06-16 08:30:44 +0530

    Done with the Followers and following page

--- src/pages/Followers.tsx
+++ src/pages/Followers.tsx
@@ -3,25 +3,31 @@ import Sidebar from "../components/Sidebar";
 import Users from "../components/Users";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import RightSideBar from "../components/RightSideBar";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 const Followers = () => {
   const { userId } = useParams();
   return (
-    <main className="flex min-h-full flex-1 px-30">
-      <Sidebar />
-      <section className="w-xl border-x-1 border-gray-400">
-        <div className="flex items-center gap-2 px-4 py-2 text-xl font-bold text-sky-950">
-          <Link
-            to={`/user/${userId}`}
-            className="rounded-full p-2 hover:bg-gray-200"
-          >
-            <ArrowLeftIcon className="h-5 w-5" />
-          </Link>
-          Followers
-        </div>
-        <Users type="followers" />
-      </section>
-      <RightSideBar />
-    </main>
+    <>
+      <Header />
+      <main className="flex min-h-full flex-1 xl:px-16 2xl:px-32">
+        <Sidebar />
+        <section className="max-w-xl flex-1/10 border-gray-400 sm:border-x-1">
+          <div className="flex items-center gap-2 px-4 py-2 text-xl font-bold text-sky-950">
+            <Link
+              to={`/user/${userId}`}
+              className="rounded-full p-2 hover:bg-gray-200"
+            >
+              <ArrowLeftIcon className="h-5 w-5" />
+            </Link>
+            Followers
+          </div>
+          <Users type="followers" />
+        </section>
+        <RightSideBar />
+      </main>
+      <Footer/>
+    </>
   );
 };
 
--- src/pages/Following.tsx
+++ src/pages/Following.tsx
@@ -3,26 +3,32 @@ import Sidebar from "../components/Sidebar";
 import Users from "../components/Users";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import RightSideBar from "../components/RightSideBar";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 const Following = () => {
   const { userId } = useParams();
   return (
-    <main className="flex min-h-full flex-1 px-30">
-      <Sidebar />
-      <section className="w-xl border-x-1 border-gray-400">
-        <div className="flex items-center gap-2 px-4 py-2 text-xl font-bold text-sky-950">
-          <Link
-            to={`/user/${userId}`}
-            className="rounded-full p-2 hover:bg-gray-200"
-          >
-            <ArrowLeftIcon className="h-5 w-5" />
-          </Link>
-          Following
-        </div>
+    <>
+      <Header />
+      <main className="flex min-h-full flex-1 xl:px-16 2xl:px-32">
+        <Sidebar />
+        <section className="max-w-xl flex-1/10 border-gray-400 sm:border-x-1">
+          <div className="flex items-center gap-2 px-4 py-2 text-xl font-bold text-sky-950">
+            <Link
+              to={`/user/${userId}`}
+              className="rounded-full p-2 hover:bg-gray-200"
+            >
+              <ArrowLeftIcon className="h-5 w-5" />
+            </Link>
+            Following
+          </div>
 
-        <Users type="following" />
-      </section>
-      <RightSideBar />
-    </main>
+          <Users type="following" />
+        </section>
+        <RightSideBar />
+      </main>
+      <Footer />
+    </>
   );
 };
 
